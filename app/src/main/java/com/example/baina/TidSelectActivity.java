956664dd74810d1e12db067cc9dd573d5c092af0
salalaiji <1109435501@qq.com>
2016-06-10 16:23:37 +0800

    设计车次选择，车站选择，站站选择，添加车次页面

--- app/src/main/java/com/example/baina/TidSelectActivity.java
+++ app/src/main/java/com/example/baina/TidSelectActivity.java
@@ -3,16 +3,11 @@ package com.example.baina;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TidSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void clickAbout()
-    {
-
+        setContentView(R.layout.activity_tid_select);
     }
 }
