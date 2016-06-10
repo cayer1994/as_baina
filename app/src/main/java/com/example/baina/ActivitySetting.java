73be2d7ce1f44f3e4b573720b6769530a5b815dd
salalaiji <1109435501@qq.com>
2016-06-10 19:51:18 +0800

    activity的页面设计基本完成。接下来开始sqlite数据库设计

---
+++ app/src/main/java/com/example/baina/ActivitySetting.java
@@ -0,0 +1,13 @@
+package com.example.baina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_setting);
+    }
+}
