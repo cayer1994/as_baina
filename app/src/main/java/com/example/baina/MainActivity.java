c6c92aa899c63da6a8b479cd32faca073be1b947
salalaiji <1109435501@qq.com>
2016-06-11 19:27:05 +0800

    补充activity，尝试Intent进行无传输数据跳转，复制旧的sqlite操作

--- app/src/main/java/com/example/baina/MainActivity.java
+++ app/src/main/java/com/example/baina/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.baina;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void clickAbout()
+    //切换到About界面
+    public void clickAbout(View v)
     {
-
+        Intent i=new Intent(MainActivity.this,AboutActivity.class);
+        startActivity(i);
     }
+
+    //切换到车次查询界面
+    public void  clickTid(View v)
+    {
+        Intent i=new Intent(MainActivity.this,TidSelectActivity.class);
+        startActivity(i);
+    }
+
+    //切换到车站查询界面
+    public void clickStation(View v)
+    {
+        Intent i=new Intent(MainActivity.this,StationActivity.class);
+        startActivity(i);
+    }
+
+    //Go to StationAndStationSelectActivity
+
+    public  void  clickSAS(View v)
+    {
+    Intent i=new Intent(MainActivity.this,StationAndStationSelectActivity.class);
+        startActivity(i);
+    }
+    //Go to MethodActivity
+    public  void clickMethod(View v)
+    {
+        Intent i=new Intent(MainActivity.this,MethodActivity.class);
+        startActivity(i);
+        }
+
+    //Go to ActivitySetting
+    public  void clickSetting(View v)
+    {
+        Intent i=new Intent(MainActivity.this,ActivitySetting.class);
+        startActivity(i);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/baina/ViewModel.java
@@ -0,0 +1,16 @@
+package com.example.baina;
+
+/**
+ * Created by 刘宏宇 on 2016/6/11.
+ */
+public class ViewModel {
+    public String id;
+    public String name;
+    public String startstation;//始发站
+    public String arrivestation;//终点站
+    public String type;
+    public String beginstation;//起始站
+    public String begintime;
+    public String stopstation;//到站
+    public String stoptime;
+}
